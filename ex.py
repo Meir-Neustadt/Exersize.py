a0bfcbd644d84a74a5bf308ba51952ea8b36fbe7
MeirNeustadt <msn.binah.1@gmail.com>
2024-03-27 11:31:18 +0200

    exersize 2

--- ex.py
+++ ex.py
@@ -5,7 +5,8 @@ list=[11, 22, 33, 44, 55, 66, 77, 88, 99]
 random.shuffle(list)
 num=list.pop()
 list.sort(reverse=True)
-print(*list, sep=';', end=' ') 
+print(*list, sep=';', end=' ')
+print() 
 
 num=1234.56789
 print(f"{round(num, 3):,}")
