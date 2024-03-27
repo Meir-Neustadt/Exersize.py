8b2aa52d727f42a20d935b4ddf60622582b6d80f
MeirNeustadt <msn.binah.1@gmail.com>
2024-03-27 11:30:49 +0200

    exersize 1

---
+++ ex.py
@@ -0,0 +1,11 @@
+import random
+import re
+
+list=[11, 22, 33, 44, 55, 66, 77, 88, 99]
+random.shuffle(list)
+num=list.pop()
+list.sort(reverse=True)
+print(*list, sep=';', end=' ') 
+
+num=1234.56789
+print(f"{round(num, 3):,}")
