ddd1f3127615af930d97d520ffb045161a92c147
MeirNeustadt <msn.binah.1@gmail.com>
2024-03-27 12:20:17 +0200

    exersize3

--- ex.py
+++ ex.py
@@ -2,11 +2,19 @@ import random
 import re
 
 list=[11, 22, 33, 44, 55, 66, 77, 88, 99]
+
 random.shuffle(list)
 num=list.pop()
+if 22 in list:
+    list.remove(22)
 list.sort(reverse=True)
-print(*list, sep=';', end=' ')
-print() 
+
+newList1=list[1:5:2]
+newList2=[round(num/(num+20), 2) for num in list[:5]]
+
+print(*list, sep='; ', end='.\n')
+print(*newList1, sep=', ', end=':\n')
+print(*newList2, sep=': ', end='|\n')
 
 num=1234.56789
 print(f"{round(num, 3):,}")
