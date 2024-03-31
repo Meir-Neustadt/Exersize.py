48f710605f814bb96dbff9b5e53d9aefce281b39
MeirNeustadt <msn.binah.1@gmail.com>
2024-03-31 11:35:07 +0300

    exersize 6

--- classes.py
+++ classes.py
@@ -1,16 +1,18 @@
+import statistics
 import re
+import patterns
+import func
 
-
-class member:
+class Member:
 
     count = 0
 
-    def __init__(self, lname, fname, age, mail: int):
+    def __init__(self, lname, fname, age, mail):
         self.lname=lname
         self.fname=fname
         self.age=age
         self.mail=mail
-        member.count+=1
+        Member.count+=1
     
     @property
     def lname(self):
@@ -21,19 +23,22 @@ class member:
     @property
     def age(self):
         return self._age
+    @property
+    def mail(self):
+        return self._mail
     
     @lname.setter
     def lname(self, lname):
-        if re.search(r"^\w{3,20}$", lname):
+        if re.search(patterns.name_pattern, lname):
             self._lname=lname.capitalize()
         else:
-            raise ValueError('not valid last name')
+            raise ValueError('not a valid last name')
     @fname.setter
     def fname(self, fname):
-        if re.search(r"^\w{3,20}$", fname):
+        if re.search(patterns.name_pattern, fname):
             self._fname=fname.capitalize()
         else:
-            raise ValueError('not valid first name')
+            raise ValueError('not a valid first name')
     @age.setter
     def age(self, age):
         try:
@@ -44,12 +49,44 @@ class member:
             self._age=age
         else:
             raise ValueError('not a valid age')
+    @mail.setter
+    def mail(self, mail):
+        if re.search(patterns.mail_pattern, mail):
+            self._mail=mail
+        else:
+            raise ValueError('not a valid mail')
         
     def __str__(self):
-        return(f'{self.lname} {self.fname}, {str(self.age)}')
+        return f'{self.lname} {self.fname}, {str(self.age)}, {self.mail},'
     
 
-class student(member):
-    def __init__(self, lname, fname, age: int):
-        super().__init__(lname, fname, age)
+class Student(Member):
 
+    def __init__(self, lname, fname, age, mail, grades):
+        super().__init__(lname, fname, age, mail)
+        self.grades=grades
+        self.grade=grades
+
+    @property 
+    def grades(self):
+        return self._grades
+    @property 
+    def grade(self):
+        return self._grade
+
+    @grades.setter
+    def grades(self, grades):
+        self._grades=grades
+    @grade.setter
+    def grade(self, grades):
+        self._grade=func.ave(grades, 1)
+
+    def __str__(self):
+        return super().__str__() + f'{self.grades}, {self.grade}'
+
+
+
+######################
+    
+student = Student('Meir','Neustadt',26,'msn.binah.1@gmail.com',[99,100,100,97,100,100,95,100,100,100,100])
+print(student)
---
+++ func.py
@@ -0,0 +1,2 @@
+def ave (list, after_dot):
+    return round(sum(list)/len(list), after_dot)
